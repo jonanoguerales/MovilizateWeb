d6ce47b1a5cd0b790e3d64633348f50e564fad69
Jonathan Noguerales <jonathan.n@pyv.es>
2024-03-25 11:27:47 +0100

    Cambios 5.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  NotFound: '/404',
   compilerOptions: {
     allowJs: true
   }
